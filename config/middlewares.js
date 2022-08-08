a31489345b8dff52ea97ab00f8109fc1aab46f2c
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2022-08-08 22:23:08 +0800

    git push

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,18 +9,4 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
 ];
