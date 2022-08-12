8bae678cb5f79dbe052229f2b33b8c010243d492
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2022-08-13 02:55:37 +0800

    cloudinary postgres added

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-const path = require('path');
+// const path = require('path');
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+// strapi-api/config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'ec2-34-193-44-192.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'd5hm1417brof7l'),
+      user: env('DATABASE_USERNAME', 'uvrffcuwrjwewi'),
+      password: env('DATABASE_PASSWORD', '9f31ad5e1914bdfaefed31c43188e8138660983abd1e96c8b44b20d67e8f157c'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
